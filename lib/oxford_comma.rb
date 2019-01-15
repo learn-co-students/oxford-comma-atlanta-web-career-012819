87278f25fb06040ab274a5405596e6bb486c4d9b
Leigh Baugham <leigh.baugham@gmail.com>
2019-01-15 17:16:15 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.length == 1
+  [array].join
+
+elsif array.length == 2
+      "#{array[0]} and #{array[1]}"
+
+elsif array.length == 3
+
+      array[-1].insert(0, "and ")
+      array.join(", ")
+
+
+else array.length > 3
+
+  deleted = array.pop
+  array.join(", ") + ", and " + "#{deleted}"
+    end
 end
